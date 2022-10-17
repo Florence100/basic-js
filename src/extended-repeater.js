21422e42f60f0d918b351dc143d6bd86373d5b2e
anastasia <anastasia.gribovskaia@gmail.com>
2022-10-17 14:18:58 +0300

    new task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let repeatString = '';
+  let resultString = '';
+  let separator = options.separator || '+';
+  let repeatTimes = options.repeatTimes || 0;
+  console.log(String(options.addition))
+  let addition;
+  if (String(options.addition) === 'false') {
+    addition = 'false';
+  } else {
+    addition = (options.addition === null) ? 'null' : options.addition || '';
+  }
+
+  let additionRepeatTimes = options.additionRepeatTimes || 0;
+  let additionSeparator = options.additionSeparator || "|";
+  let separatorLength = separator.length;
+  let additionSeparatorLength = additionSeparator.length;
+  
+  str = String(str);
+  console.log(addition)
+
+  if (additionRepeatTimes > 0) {
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      repeatString = repeatString + addition + additionSeparator;
+    }
+  } else {
+    repeatString = addition + additionSeparator;
+  }
+  repeatString = repeatString.slice(0, repeatString.length - additionSeparatorLength)
+
+  let repeatResultString = str + repeatString + separator;
+
+  if (repeatTimes > 0) {
+    for (let i = 0; i < repeatTimes; i++) {
+    resultString += repeatResultString;
+    }
+  } else {
+    resultString = repeatResultString;
+  }
+
+  return resultString.slice(0, resultString.length - separatorLength);
 }
 
 module.exports = {
   repeater
 };
+
+
+console.log(repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }))
